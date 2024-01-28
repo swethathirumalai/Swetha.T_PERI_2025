dd9809ca34fb0b592e2d39368c7801c9bbb75965
swethathirumalai <147401773+swethathirumalai@users.noreply.github.com>
2024-01-28 22:33:47 +0530

    Create Question 1.py

---
+++ 26-01-24/Question 1.py
@@ -0,0 +1,52 @@
+'' Kristen loves playing with and comparing numbers. She thinks that if she takes two different positive numbers, the one whose digits sum to a larger number is better than the other. If the sum of digits is equal for both numbers, then she thinks the smaller number is better. For example, Kristen thinks that  is better than  and that  is better than .
+
+Given an integer, , can you find the divisor of  that Kristin will consider to be the best?
+
+Input Format
+
+A single integer denoting .
+
+Constraints
+
+Output Format
+
+Print an integer denoting the best divisor of .
+
+Sample Input 0
+
+12
+Sample Output 0
+
+6
+Explanation 0
+
+The set of divisors of  can be expressed as . The divisor whose digits sum to the largest number is  (which, having only one digit, sums to itself). Thus, we print  as our answer. '''
+
+
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    d=[]
+    s=[]
+    for i in range(1,n+1):
+        if n%i==0:
+            d.append(str(i))
+    for i in d:
+        k=0
+        for j in i:
+            k+=int(j)
+        s.append(k)
+    u=max(s)
+    y=s.index(u)
+    print(d[y])
