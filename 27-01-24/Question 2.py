0a22d1785a7b8daedb69f99f4ade02be35976556
swethathirumalai <147401773+swethathirumalai@users.noreply.github.com>
2024-01-28 23:05:11 +0530

    Create Question 2.py

---
+++ 27-01-24/Question 2.py
@@ -0,0 +1,75 @@
+"""
+Two words are anagrams of one another if their letters can be rearranged to form the other word.
+
+Given a string, split it into two contiguous substrings of equal length. Determine the minimum number of characters to change to make the two substrings into anagrams of one another.
+
+Example
+
+Break  into two parts: 'abc' and 'cde'. Note that all letters have been used, the substrings are contiguous and their lengths are equal. Now you can change 'a' and 'b' in the first substring to 'd' and 'e' to have 'dec' and 'cde' which are anagrams. Two changes were necessary.
+
+Function Description
+
+Complete the anagram function in the editor below.
+
+anagram has the following parameter(s):
+
+string s: a string
+Returns
+
+int: the minimum number of characters to change or -1.
+Input Format
+
+The first line will contain an integer, , the number of test cases.
+Each test case will contain a string .
+
+Constraints
+
+
+ consists only of characters in the range ascii[a-z].
+Sample Input
+
+6
+aaabbb
+ab
+abc
+mnop
+xyyx
+xaxbbbxx
+Sample Output
+
+3
+1
+-1
+2
+0
+1
+
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'anagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def anagram(s):
+    length = len(s)
+    half_length = len(s)//2
+   
+    n1 = list(s[:half_length])
+    n2 = list(s[half_length:])
+    
+    if length % 2 != 0:
+        return -1
+    
+    count = 0
+    for i in range(len(n1)):
+            i
